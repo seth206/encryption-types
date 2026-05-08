5d40e810a89d51a89fcca99c57c8d3823007cf32
seth206 <25sbecker@silverdale.chorustrust.org>
2026-05-08 13:10:44 +0100

    Update xor.js

--- xor.js
+++ xor.js
@@ -1,7 +1,9 @@
-
 export function encrypt(text, key) {
+  if (!key) return "Key required";
   return [...text]
-    .map((c, i) => String.fromCharCode(c.charCodeAt(0) ^ key.charCodeAt(i % key.length)))
+    .map((c, i) =>
+      String.fromCharCode(c.charCodeAt(0) ^ key.charCodeAt(i % key.length))
+    )
     .join("");
 }
 
