a687e72aa7bc25d1fd50db8faadbefabe3d1ee6b
seth206 <25sbecker@silverdale.chorustrust.org>
2026-05-08 13:10:27 +0100

    Update caesar.js

--- caesar.js
+++ caesar.js
@@ -1,12 +1,14 @@
-
-const shiftChar = (c, shift) => {
+function shiftChar(c, shift) {
   const code = c.charCodeAt(0);
+
   if (code >= 65 && code <= 90)
     return String.fromCharCode(((code - 65 + shift) % 26) + 65);
+
   if (code >= 97 && code <= 122)
     return String.fromCharCode(((code - 97 + shift) % 26) + 97);
+
   return c;
-};
+}
 
 export function encrypt(text, shift) {
   shift = Number(shift) || 0;
